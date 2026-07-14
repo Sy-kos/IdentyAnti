e52b05ba0dd7e256d647f3f4f99ded34f97e337a
Sy-kos <karina.m.bastias@gmail.com>
2026-07-14 09:04:17 -0400

    Update Prototipo final.py

--- Prototipo final.py
+++ Prototipo final.py
@@ -129,7 +129,10 @@ resultados_enzima = datos[COLUMNA_ENZIMA] if usar_enzimas else None
     # ============================
     # EJECUCIÓN DE LA LÓGICA CLÍNICA
     # ============================
-
+     antig_confirmar_u = None
+     confirmar_mezcla = None
+     conclusion = None
+     controles = []
     # Filtro de descarte
     antigenos_descartados = set()
     celulas_negativas = datos[resultados_paciente == 0]
